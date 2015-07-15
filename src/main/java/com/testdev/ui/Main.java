181810970a823aa96e0ec89e5d128e5eedd20559
Oleh Krupenia <oleh.krupenia@globallogic.com>
2015-07-15 16:58:16 +0300

    Adding of the label

--- src/main/java/com/testdev/ui/Main.java
+++ src/main/java/com/testdev/ui/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
         primaryStage.setTitle("Tic Tac Toe");
-        primaryStage.setScene(new Scene(root, 300, 300));
+        primaryStage.setScene(new Scene(root, 300, 330));
         primaryStage.show();
     }
 
