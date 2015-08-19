01a1146dd5e3e29521e439246f8737e4dfdee348
Oleh Krupenia <oleh.krupenia@globallogic.com>
2015-08-19 20:19:23 +0300

    Extracting of the constants into enum

---
+++ src/main/java/com/testdev/service/field/FieldValidationResult.java
@@ -0,0 +1,19 @@
+package com.testdev.service.field;
+
+/**
+ * Represents the result of the field validation after each move.
+ * 0 - if there is no winner yet and it is possible to continue the game. 1 - if the winner is player 1.
+ * 2 - if the winner is player 2. 3 - if there is a tie game.
+ */
+public enum FieldValidationResult {
+    EMPTY_CELLS_EXIST(0), PLAYER_ONE_WON(1), PLAYER_TWO_WON(2), TIE_GAME(3);
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    FieldValidationResult(int value) {
+        this.value = value;
+    }
+}
